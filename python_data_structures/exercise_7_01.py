5029633363770f280dc37c0975056ecb588bffe6
[lucadm] <[lucadimaio@gmail.com]>
2025-10-01 17:55:19 +0100

    Add useful comment notes to exercise 7

--- python_data_structures/exercise_7_01.py
+++ python_data_structures/exercise_7_01.py
@@ -1,7 +1,9 @@
 '''Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case. Use the file words.txt to produce the output below.
 You can download the sample data at http://www.py4e.com/code3/words.txt'''
 
+#first open a connection to the file with open()
 file_name = open("words.txt")
+#read the file in form of a sequence of strings with \n character separating each line
 read_file_name = file_name.read()
 file_upper = read_file_name.upper()
 print(file_upper)
